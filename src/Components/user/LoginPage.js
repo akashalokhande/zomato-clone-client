6d9b57fedb083e601444abdce1b2f8ccb3898d61
AkashLokhande <akashlok97@gmail.com>
2024-01-29 20:07:55 +0530

    css-changes

--- src/Components/user/LoginPage.js
+++ src/Components/user/LoginPage.js
@@ -5,19 +5,20 @@ import Swal from "sweetalert2";
 import { GoogleLogin } from "@react-oauth/google";
 import Header from "../common/Header";
 
-
 export default function LoginPage() {
   let [user, setUser] = useState(null);
 
   let onSuccess = (response) => {
-    localStorage.setItem("auth_token", response.credential); 
+    localStorage.setItem("auth_token", response.credential);
     Swal.fire({
       position: "center",
       icon: "success",
       title: "Login Successful!",
       showConfirmButton: false,
       timer: 1500,
-    }).then(() => window.location.assign(`/quick-search/${localStorage.getItem("meal_id")}`));
+    }).then(() =>
+      window.location.assign(`/quick-search/${localStorage.getItem("meal_id")}`)
+    );
   };
 
   let onError = () => {
@@ -35,13 +36,14 @@ export default function LoginPage() {
   }, []);
   return (
     <>
-      <Header bg="bg-danger" login="d-none"  />
-      <div className="d-flex justify-content-between align-content-center marginSearch  mt-5 Poppins fw-bolder ">
-        <h1>Login with Google to continue</h1>
-      </div>
-      <div className="d-flex justify-content-center shadow marginLeft col-7   mt-5 ">
+      <Header bg="bg-danger" login="d-none" />
+      <div className=" d-flex justify-content-center mt-5 Poppins fw-bolder">
+        <h1 className="login-center">Login with Google to continue</h1>
+        </div>
+      <section className="d-flex justify-content-center align-items-center" >
+      <div className="d-flex justify-content-center align-items-center shadow  col-7  mt-5 ">
         <GoogleOAuthProvider clientId="960137283931-7pijg3qdkd762o9c4d0ov8njrh5e0ri7.apps.googleusercontent.com">
-          <div className=" d-flex flex-column align-items-center py-3">
+          <div className=" d-flex align-items-center py-3">
             {
               <GoogleLogin
                 onSuccess={(credentialResponse) => {
@@ -55,6 +57,7 @@ export default function LoginPage() {
           </div>
         </GoogleOAuthProvider>
       </div>
+      </section>
     </>
   );
 }
