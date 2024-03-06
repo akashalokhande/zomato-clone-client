36fbe84eff13ad5451f7b08512c9b0f513f7e1be
AkashLokhande <akashlok97@gmail.com>
2024-03-06 17:58:42 +0530

    changes

--- src/Components/common/MyOrder.js
+++ src/Components/common/MyOrder.js
@@ -5,10 +5,10 @@ import Header from "./Header";
 function MyOrder() {
   let [orders, setorder] = useState([]);
   let getmyorder = async () => {
-   let user_email = localStorage.getItem("email");
-   console.log(user_email);
+    let user_email = localStorage.getItem("email");
     let url = `https://zomato-web-clone.onrender.com/api/my-order/${user_email}`;
     let { data } = await axios.get(url);
+    console.log(data.my_order);
     setorder(data.my_order);
   };
 
@@ -18,21 +18,42 @@ function MyOrder() {
 
   return (
     <>
-    <Header bg="bg-danger"/>
-    <div className="my-orders-container">
-      <h2 className="my-orders-heading">My Orders</h2>
-      <div className="order-list">
-        {orders.map((order) => (
-          <div key={order._id} className="order-item">
-            <div className="order-details">
-              <p className="order-name">{order.order_list[0].name}</p>
-              <p className="order-name">{order.order_list[0].qty}</p>
-              <p className="order-price">Total:₹{order.totalAmount}</p>
-            </div>
+      <div>
+        <Header bg="bg-danger" />
+        <div className="my-orders-container">
+          {/* <h2 className="my-orders-heading">My Orders</h2> */}
+          <div className="order-list my-3">
+            {orders.map((order) => (
+              <div key={order._id} className="order-item bg-success text-white">
+                <div className="order-details">
+                  {order.order_list.map((list) => (
+                    <>
+                      <div className="bg-secondary p-3 rounded-2 my-2">
+                        <div className="d-flex justify-content-between">
+                          <p className="order-name">{list.name}</p>
+                          <img
+                            src={"/images/" + list.image}
+                            alt=""
+                            className="order-image"
+                          />
+                        </div>
+                        <p className="d-flex justify-content-between pt-2">
+                          <span>Price:₹{list.price}</span>
+                          <span className="pe-2">Qty:{list.qty}</span>
+                        </p>
+                      </div>
+                    </>
+                  ))}
+
+                  <div className="d-flex justify-content-center ">
+                    <p className="order-name">Total:₹{order.totalAmount}</p>{" "}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
     </>
   );
 }
