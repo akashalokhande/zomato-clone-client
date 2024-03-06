3dddb4c04390fa809e5e45e6f0400c47cd9f476a
AkashLokhande <akashlok97@gmail.com>
2024-03-06 20:16:59 +0530

    changes

--- src/Components/common/MyOrder.js
+++ src/Components/common/MyOrder.js
@@ -20,39 +20,31 @@ function MyOrder() {
     <>
       <div>
         <Header bg="bg-danger" />
-        <div className="my-orders-container">
-          {/* <h2 className="my-orders-heading">My Orders</h2> */}
-          <div className="order-list my-3">
-            {orders.map((order) => (
-              <div key={order._id} className="order-item bg-success text-white">
-                <div className="order-details">
-                  {order.order_list.map((list) => (
-                    <>
-                      <div className="bg-secondary p-3 rounded-2 my-2">
-                        <div className="d-flex justify-content-between">
-                          <p className="order-name">{list.name}</p>
-                          <img
-                            src={"/images/" + list.image}
-                            alt=""
-                            className="order-image"
-                          />
-                        </div>
-                        <p className="d-flex justify-content-between pt-2">
-                          <span>Price:₹{list.price}</span>
-                          <span className="pe-2">Qty:{list.qty}</span>
-                        </p>
-                      </div>
-                    </>
-                  ))}
 
-                  <div className="d-flex justify-content-center ">
-                    <p className="order-name">Total:₹{order.totalAmount}</p>{" "}
+        {orders.map((order) => (
+          <div className="my-orders-container">
+            <div className="my-3">
+              {order.order_list.map((list) => (
+                <div className="order-item bg-success text-white">
+                  <div className="order-details">
+                    <div className="d-flex justify-content-between">
+                      <p className="order-name">{list.name}</p>
+                      <img
+                        src={"/images/" + list.image}
+                        alt=""
+                        className="order-image"
+                      />
+                    </div>
+                    <p className="d-flex justify-content-between pt-2">
+                      <span>Price:₹{list.price}</span>
+                      <span className="pe-2">Qty:{list.qty}</span>
+                    </p>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
